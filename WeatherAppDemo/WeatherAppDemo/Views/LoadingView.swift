3c95ca3a906dee695ad38a9673d06141b926729e
HiSmile <tranhiepmdc@gmail.com>
2023-02-28 14:35:39 +0700

    feat: done part I weather app: init project, welcome screen, get currentLocation

---
+++ WeatherAppDemo/WeatherAppDemo/Views/LoadingView.swift
@@ -0,0 +1,21 @@
+//
+//  ProgressView.swift
+//  WeatherAppDemo
+//
+//  Created by Newwave on 28/02/2023.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView().progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
