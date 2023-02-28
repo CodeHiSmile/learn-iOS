5307e3dd9e1d5a0b0437f27d8663f85b737483b8
HiSmile <tranhiepmdc@gmail.com>
2023-02-28 15:28:15 +0700

    feat: done weather part II: get api weather

---
+++ WeatherAppDemo/WeatherAppDemo/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  WeatherAppDemo
+//
+//  Created by Newwave on 28/02/2023.
+//
+
+import Foundation
+
+extension Double{
+    public func roundDouble() -> String {
+        return String(format: "%.0f", self)
+    }
+}
---
+++ WeatherAppDemo/WeatherAppDemo/Views/WeatherView.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherView.swift
+//  WeatherAppDemo
+//
+//  Created by Newwave on 28/02/2023.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var weather: ResponseBody
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView(weather: previewWeather)
+    }
+}
